83e987459ecc889129150ab5fae4ac386f8400c0
NoelPrangley <noel@inferencelabs.com>
2024-08-20 10:43:19 -0400

    Update and rename test27.sh to test28.sh

--- test28.sh
+++ test28.sh
@@ -8,13 +8,9 @@ elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
 fi
 i=$((i+1)); 
 done'
-chmod 666 test27.sh
-echo " Write in your age: "
-read age 
-
-if [[ "$age" -le 7 || "$age" -ge 65 ]] ; then
-    echo " You can walk in for free "
-elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
-    echo " You have to pay for ticket "
-fi
+i=1
+while [[ $i -le 10 ]] ; do
+   echo "$i"
+  (( i += 1 ))
+done
  
