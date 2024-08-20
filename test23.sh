ae846042226ddc8fb773a15d2fbf822039f55d52
NoelPrangley <noel@inferencelabs.com>
2024-08-20 10:20:09 -0400

    Update and rename test22.sh to test23.sh

--- test23.sh
+++ test23.sh
@@ -9,11 +9,12 @@ fi
 i=$((i+1)); 
 done'
 
-echo "What is your name?" 
+echo " Write in your age: "
+read age
 
-read entered_name 
-
-echo -e "\nWelcome to bash tutorial" $entered_name
-
-
+if [[ "$age" -le 7 || "$age" -ge 65 ]] ; then
+    echo " You can walk in for free "
+elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
+    echo " You have to pay for ticket "
+fi
  
