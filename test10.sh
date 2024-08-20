ce6f81db8b415b1d662a1d1654e2e37984a7b47b
NoelPrangley <noel@inferencelabs.com>
2024-08-20 09:52:45 -0400

    Update and rename test9.sh to test10.sh

--- test10.sh
+++ test10.sh
@@ -3,9 +3,9 @@
 echo 'i=1; 
 for age in $@; 
 if [[ "$age" -le 7 || "$age" -ge 65 ]] ; then
-    echo " You can walk in for free "
+    do echo " You can walk in for free "
 elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
-    echo " You have to pay for ticket "
+    do echo " You have to pay for ticket "
 fi
 i=$((i+1)); 
 done'
