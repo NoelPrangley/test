7cce9e21dc5e8f29518d1f9d1f8e5046932b2da9
NoelPrangley <noel@inferencelabs.com>
2024-08-20 11:37:07 -0400

    Update and rename test_b1.sh to test_b2.sh

--- test_b2.sh
+++ test_b2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-i=1;
+echo 'i=1;
 for age in $@; 
 do if [[ "$age" -le 7 || "$age" -ge 65 ]] ; then
     echo " You can walk in for free "
 elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
     echo " You have to pay for ticket "
 fi
-i=$((i+1)); done
+i=$((i+1)); done'
+
+
