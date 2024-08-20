59ac40c4c87ef859497273c69150dc8b74eec8ed
NoelPrangley <noel@inferencelabs.com>
2024-08-20 10:47:27 -0400

    Update and rename test30.sh to test31.sh

--- test31.sh
+++ test31.sh
@@ -6,5 +6,12 @@ do if [[ "$age" -le 7 || "$age" -ge 65 ]] ; then
 elif [[ "$age" -gt 7 && "$age" -lt 65 ]] ; then
     echo " You have to pay for ticket "
 fi
-i=$((i+1));
-done'
+i=$((i+1));'
+
+b=1
+while [[ $b -le 10 ]] ; do
+   echo "$b"
+  (( b += 1 ))
+done
+ 
+
